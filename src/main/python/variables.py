a3953f127f772e74c5ce72bb73036c56f7dfb945
raksha bhati <rakshabhatirb@gmail.com>
2024-02-23 15:33:27 +0530

    test

--- src/main/python/variables.py
+++ src/main/python/variables.py
@@ -1,6 +1,4 @@
-"""Variables are containers for storing data values.
-Python has no command for declaring a variable.
-A variable is created the moment you first assign a value to it."""
+"""Variables are containers for storing data values.Python has no command for declaring a variable.A variable is created the moment you first assign a value to it."""
 x = 5
 y = "John"
 print(x, y)
